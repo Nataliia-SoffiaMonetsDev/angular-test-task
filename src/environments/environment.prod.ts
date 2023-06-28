1084c0b3434d97ba5dc9eb47ce319b7f2d2bc566
Nataliia-SoffiaMonetsDev <nataliiasofiia.monets.dev@gmail.com>
2023-06-28 18:58:39 +0300

    changed back port

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    apiUrl: 'http://ec2-16-171-40-193.eu-north-1.compute.amazonaws.com:5000'
+    apiUrl: 'http://ec2-13-51-160-81.eu-north-1.compute.amazonaws.com:5000'
 };
