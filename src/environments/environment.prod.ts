ddf53a5807cb9e8478eec93874745447060c8033
Nataliia-SoffiaMonetsDev <nataliiasofiia.monets.dev@gmail.com>
2023-06-28 13:41:01 +0300

    changed back url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-    production: true
+    production: true,
+    apiUrl: 'http://ec2-16-171-40-193.eu-north-1.compute.amazonaws.com:5000'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://localhost:5000'
+    apiUrl: 'http://ec2-16-171-40-193.eu-north-1.compute.amazonaws.com:5000'
 };
