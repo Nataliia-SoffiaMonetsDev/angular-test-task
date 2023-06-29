32f386eca6a473726f1334b823d984c3c2cb9ed9
Nataliia-SoffiaMonetsDev <nataliiasofiia.monets.dev@gmail.com>
2023-06-29 11:20:48 +0300

    updated dist and api url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    apiUrl: 'http://ec2-13-51-160-81.eu-north-1.compute.amazonaws.com:5000'
+    apiUrl: 'http://ec2-13-48-203-52.eu-north-1.compute.amazonaws.com:5000'
 };
