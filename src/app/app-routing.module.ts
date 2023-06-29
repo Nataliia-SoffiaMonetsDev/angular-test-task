df8c428ad8d6459e8989d6633a2c25a8931f7f98
Nataliia-SoffiaMonetsDev <nataliiasofiia.monets.dev@gmail.com>
2023-06-29 15:27:40 +0300

    deleted unnecessary imports

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LoginComponent } from './pages/login/login.component';
-import { ProductsPageComponent } from './pages/products-page/products-page.component';
-import { ProductDetailsComponent } from './pages/product-details/product-details.component';
-import { RegistrationComponent } from './pages/registration/registration.component';
 import { AuthGuard } from './_guards/auth.guard';
 
 const routes: Routes = [
